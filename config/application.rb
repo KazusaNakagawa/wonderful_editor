94fee2bfb923c62cd0111f7d8467b844270a1789
KazusaNakagawa <kazusanakgawa@gmail.com>
2020-10-04 20:02:22 +0900

    generator cmd の設定,  testは rspecを基本に

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,10 @@ module WonderfulEditor
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
+    config.generation do |g|
+      g.test_framwork :rspec
+    end
+
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
