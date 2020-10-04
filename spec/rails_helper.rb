93453a8a42269831dcfc2f58fa919472ee8a3f87
KazusaNakagawa <kazusanakgawa@gmail.com>
2020-10-04 20:01:05 +0900

    rails_helperの設定, 言語, 省略形

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,9 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
+# set langage
+Faker::Config.locale = :ja
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -61,4 +64,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # FaxtoryBotのmethodを省略した形で呼び出せる
+  config.include FactoryBot::Syntax::Methods
 end
