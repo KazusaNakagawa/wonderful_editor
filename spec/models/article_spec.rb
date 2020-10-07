be6d3aac2c3e84e0bd59908f690ae4463c03ac62
KazusaNakagawa <kazusanakgawa@gmail.com>
2020-10-07 20:30:59 +0900

    db:migrate を実行

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :bigint           not null, primary key
+#  body       :text
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_articles_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 require "rails_helper"
 
 RSpec.describe Article, type: :model do
