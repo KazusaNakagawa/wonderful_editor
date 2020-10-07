dcd1a59e211b2d0afd7e0d123ab03fef0a8e1745
KazusaNakagawa <kazusanakgawa@gmail.com>
2020-10-07 18:49:47 +0900

    article_like model も生成

---
+++ app/models/article_like.rb
@@ -0,0 +1,4 @@
+class ArticleLike < ApplicationRecord
+  belongs_to :user
+  belongs_to :article
+end
---
+++ spec/factories/article_likes.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :article_like do
+    user { nil }
+    article { nil }
+  end
+end
