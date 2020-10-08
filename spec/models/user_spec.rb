6b92292b80e5de860110b8776196612db0afc15d
KazusaNakagawa <kazusanakgawa@gmail.com>
2020-10-08 21:34:06 +0900

    user model test : user が作られるテストはok

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "account を指定しているとき" do
+    it "ユーザーが作られる" do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+  end
+
+  context "account を指定していないとき" do # rubocop:disable RSpec/RepeatedExampleGroupBody
+    it "ユーザー作成に失敗する" do
+    end
+  end
+
+  context "まだ同じ名前の account が存在しないとき" do
+    it "ユーザーが作られる" do
+    end
+  end
+
+  context "すでに同じ名前の account が存在しているとき" do # rubocop:disable RSpec/RepeatedExampleGroupBody
+    it "ユーザー作成に失敗する" do
+    end
+  end
 end
