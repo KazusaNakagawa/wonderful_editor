b81f68dab9a7f7fdd59e4cc8f82fc6ec19a8f454
KazusaNakagawa <kazusanakgawa@gmail.com>
2020-10-09 07:24:51 +0900

    user model test aggregate_failures を追記

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,24 +1,39 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  context "account を指定しているとき" do
+  context "password を指定しているとき" do
     it "ユーザーが作られる" do
       user = build(:user)
       expect(user).to be_valid
     end
   end
 
-  context "account を指定していないとき" do # rubocop:disable RSpec/RepeatedExampleGroupBody
+  context "password を指定していないとき" do
     it "ユーザー作成に失敗する" do
+      user = build(:user, password: nil)
+      aggregate_failures "最後まで通過" do
+        expect(user).to be_invalid
+        expect(user.errors.details[:password][0][:error]).to eq :blank
+      end
     end
   end
 
-  context "まだ同じ名前の account が存在しないとき" do
+  context "email を指定していないとき" do
+    it "ユーザー作成に失敗する" do
+      user = build(:user, email: nil)
+      aggregate_failures "最後まで通過" do
+        expect(user).to be_invalid
+        expect(user.errors.details[:email][0][:error]).to eq :blank
+      end
+    end
+  end
+
+  context "まだ同じ名前の name が存在しないとき" do
     it "ユーザーが作られる" do
     end
   end
 
-  context "すでに同じ名前の account が存在しているとき" do # rubocop:disable RSpec/RepeatedExampleGroupBody
+  context "すでに同じ名前の name が存在しているとき" do
     it "ユーザー作成に失敗する" do
     end
   end
