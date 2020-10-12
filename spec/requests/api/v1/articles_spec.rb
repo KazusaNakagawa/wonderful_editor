eb1d8cd653b3c7b21ab3430c4b9a54d2902f4521
KazusaNakagawa <kazusanakgawa@gmail.com>
2020-10-13 08:25:54 +0900

    rubocop 修正 status is "ok"

--- spec/requests/api/v1/articles_spec.rb
+++ spec/requests/api/v1/articles_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Api::V1::Articles", type: :request do
   describe "GET /api/v1/articles" do
     it "works! (now write some real specs)" do
       get api_v1_articles_index_path
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(ok)
     end
   end
 end
