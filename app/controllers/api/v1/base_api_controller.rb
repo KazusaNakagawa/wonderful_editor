bfaf11cd0b8588cc5b5d30d6785d28ef23869133
KazusaNakagawa <kazusanakgawa@gmail.com>
2020-10-21 07:04:19 +0900

    current_user を実装

--- app/controllers/api/v1/base_api_controller.rb
+++ app/controllers/api/v1/base_api_controller.rb
@@ -1,2 +1,9 @@
 class Api::V1::BaseApiController < ApplicationController
+  # ref: https://devise-token-auth.gitbook.io/devise-token-auth/usage/controller_methods#methods
+
+  def current_user
+    # 自己代入: https://docs.ruby-lang.org/ja/2.5.0/doc/spec=2foperator.html#selfassign
+    # : 仮userとして最初の登録userを使用
+    @current_user ||= User.first
+  end
 end
