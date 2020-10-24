34fbff180fbe9d84ca81d2036941e928d28aeca6
KazusaNakagawa <kazusanakgawa@gmail.com>
2020-10-24 10:16:37 +0900

    article create test実装途中

--- app/controllers/api/v1/base_api_controller.rb
+++ app/controllers/api/v1/base_api_controller.rb
@@ -4,6 +4,7 @@ class Api::V1::BaseApiController < ApplicationController
   def current_user
     # 自己代入: https://docs.ruby-lang.org/ja/2.5.0/doc/spec=2foperator.html#selfassign
     # : 仮userとして最初の登録userを使用
+    binding.pry
     @current_user ||= User.first
   end
 end
--- spec/requests/api/v1/articles_spec.rb
+++ spec/requests/api/v1/articles_spec.rb
@@ -1,9 +1,9 @@
 require "rails_helper"
 
-# chart sheet
-# bundle exec rspec spec/requests/api/v1/articles_spec.rb --tag focus
+# CheatCMD
+#   $ bundle exec rspec spec/requests/api/v1/articles_spec.rb --tag focus
 
-# type: request の test をするにで :requestとする
+# type: request の test をするので :requestとする
 RSpec.describe "Api::V1::Articles", type: :request do
   # test対象のurl
   describe "GET /articles" do
@@ -19,10 +19,10 @@ RSpec.describe "Api::V1::Articles", type: :request do
     it "記事一覧が表示できる" do
       # >>> console sample
       # いろいろ試す事ができる
-
+      #
       # res.map {|d| d["user"]["id"] }
       # => [320, 317, 316]
-
+      #
       # article1
       # => #<Article:0x00007fed2f5a0360
       #  id: 195,
@@ -31,10 +31,10 @@ RSpec.describe "Api::V1::Articles", type: :request do
       #  user_id: 318,
       #  created_at: Fri, 16 Oct 2020 21:53:59 UTC +00:00,
       #  updated_at: Thu, 15 Oct 2020 21:53:59 UTC +00:00>
-
+      #
       # expect(res[0]["user"].keys).to eq ["id", "name", "email"]
       # => true
-
+      #
       # res
       # => [{"id"=>197,
       #   "title"=>"1yjy58z6qpjtl5il6rg3kiyr6p8r1",
@@ -48,7 +48,7 @@ RSpec.describe "Api::V1::Articles", type: :request do
       #   "title"=>"sn9un5jypo90w",
       #   "updated_at"=>"2020-10-16T21:53:58.671Z",
       #   "user"=>{"id"=>316, "name"=>"金子 優斗", "email"=>"10_solange.schulist@littel.biz"}},]
-
+      #
       # res[0]
       # => {"id"=>197,
       #  "title"=>"1yjy58z6qpjtl5il6rg3kiyr6p8r1",
@@ -98,6 +98,61 @@ RSpec.describe "Api::V1::Articles", type: :request do
 
       it "指定したid の記事が表示できない" do
         expect { subject }.to raise_error ActiveRecord::RecordNotFound
+      end
+    end
+  end
+
+  describe "POST /articles" do
+    # 2: 定義
+    #   params: xxx
+    #     xxx: >>> 送りたい値
+    subject { post(api_v1_articles_path, params: params) }
+
+    context "login している user で記事を作成する時" do
+
+      # 3: subject から呼ばれる
+      #   ref: https://qiita.com/morrr/items/f1d3ac46b029ccddd017#db%E3%81%B8%E3%81%AE%E4%BF%9D%E5%AD%98%E7%8A%B6%E6%85%8B%E3%82%92%E5%A4%89%E3%81%88%E3%81%A6%E7%94%9F%E6%88%90%E3%81%99%E3%82%8Bbuildbuild_stubbedcreateattributes_for
+      #
+      # attributes_for(:xxx) >>> FactoryBotで定義した :xxx をもとにパラメータを再生できる
+      #   [2] pry(RSpec::ExampleGroups::ApiV1Articles::POSTArticles::LogInUser)
+      #   > FactoryBot.attributes_for(:article)
+      #   => {:title=>"rehjdlj9l4fomuhpd4p2e4uzh2s31ok", :body=>"交錯済ます人口金。"}
+      #
+      # 3-1
+      #   Api::V1::ArticlesController#article_params
+      #   >> この中の params.reqire(:article)の :article を key として渡す
+      let(:params) { { article: attributes_for(:article) } }
+      binding.pry
+      # 3-2
+      #   > current_user
+      #   => #<User id: 440, provider: "email", uid: "1_levi_schowalter@lakin.info", allow_password_change: false, \
+      #   name: "竹内 翔", image: nil, email: "1_levi_schowalter@lakin.info",
+      #   created_at: "2020-10-24 00:35:36", updated_at: "2020-10-24 00:35:36">
+      let(:current_user) { create(:user) }
+
+
+      # stub: 外部APIに依存されないようにする
+      # allow().to receive(:twitter_client).and_return(current_user)
+
+
+      fit "記事が作成できる" do
+        # 1:実際の処理
+        expect { subject }.to change { Article.count }.by(1)
+        # expect { subject }.to change { User.count }.by(1)
+        binding.pry
+        # res = JSON.parse(response.body)
+        # binding.pry
+        # allow(current_user).to receive(:update)
+
+        # userがいない
+        #[1] pry(#<RSpec::ExampleGroups::ApiV1Articles::POSTArticles::LogInUser>)> params
+        # => {:title=>"803tkwz0hc9ngockjy4339bqdvqi66m5ko0j", :body=>"まほうつかいぶん脱税壮年。"}
+      end
+    end
+
+    context "login していない user が記事作成する時" do
+      it "記事が作成できない" do
+
       end
     end
   end
