779c5415073cee4709708b125e8e335d5345add3
KazusaNakagawa <kazusanakgawa@gmail.com>
2020-11-01 23:46:10 +0900

    新規登録の処理を実装

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -1,22 +1,17 @@
 class Api::V1::Auth::RegistrationsController < ApplicationController
+
   def create
-    # """ 転送されてきたデータを登録処理する """
-    user = User.new(name: params[:name],
-                    password: params[:password],
-                    email: params[:email])
+    # """転送されてきたデータを登録処理する """
+    user = User.new(user_params)
+
     user.save!
 
     render json: user
   end
 
-  # private
+  private
 
-  #   def sign_up_params
-  #     # ここに :age, :genderを追記
-  #     params.permit(:name, :email, :age, :gender, :password, :password_confirmation)
-  #   end
-
-  #   def account_update_params
-  #     params.permit(:name, :email)
-  #   end
+    def user_params
+      params.permit(:name, :password, :email)
+    end
 end
