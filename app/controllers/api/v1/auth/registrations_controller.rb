7dcbddf1e93ae6cdc89915793aa9066fba5c1f60
KazusaNakagawa <kazusanakgawa@gmail.com>
2020-11-03 12:44:57 +0900

    DeviseTokenAuthを継承して登録認証処理する

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -1,16 +1,40 @@
-class Api::V1::Auth::RegistrationsController < ApplicationController
-  def create
-    # """転送されてきたデータを登録処理する """
-    user = User.new(user_params)
-
-    user.save!
-
-    render json: user
-  end
-
+class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
   private
 
-    def user_params
-      params.permit(:name, :password, :email)
+    def sign_up_params
+      # """転送されてきたデータを認証し登録する
+      # passの値が合わないと認証が, 弾かれる
+      # """
+      #
+      # Exsample
+      #   request ----------------------------
+      #   {
+      #     "name": "name-name-json",
+      #     "email": "test@x-hack.jp",
+      #     "password": "123456",
+      #     "password_confirmation": "123456"
+      #   }
+      #
+      #   response ---------------------------
+      #   {
+      #      "status": "success",
+      #      "data": {
+      #         "id": 14,
+      #         "provider": "email",
+      #         "uid": "test@x-hack.jp",
+      #         "allow_password_change": false,
+      #         "name": "name-name-json",
+      #         "image": null,
+      #         "email": "test@x-hack.jp",
+      #         "created_at": "2020-11-03T03:33:39.917Z",
+      #         "updated_at": "2020-11-03T03:33:40.019Z"
+      #      }
+      #   }
+
+      params.permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def account_update_params
+      params.permit(:name, :email)
     end
 end
