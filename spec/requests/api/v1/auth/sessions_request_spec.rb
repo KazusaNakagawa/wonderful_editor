b8acfa972eaf96a422f20ea38cd6badf07020ca6
KazusaNakagawa <kazusanakgawa@gmail.com>
2020-11-08 20:39:55 +0900

    add ログイン機能に使用するファイル

---
+++ spec/requests/api/v1/auth/sessions_request_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "Api::V1::Auth::Sessions", type: :request do
+  describe "POST /api/v1/auth/:id" do
+    subject { post(api_v1_user_session_path, params: params) }
+
+    context "登録してるアカウントでログインする時" do
+      it "login succeeds" do
+      end
+    end
+
+    context "登録されていないアカウントでログインしようとする時" do
+      it "do Error" do
+      end
+    end
+  end
+end
