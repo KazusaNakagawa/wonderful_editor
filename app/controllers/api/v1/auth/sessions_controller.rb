494739be02b6d47b45ad62aa31a0790747341970
KazusaNakagawa <kazusanakgawa@gmail.com>
2020-11-12 00:39:02 +0900

    login機能実装中

--- app/controllers/api/v1/auth/sessions_controller.rb
+++ app/controllers/api/v1/auth/sessions_controller.rb
@@ -1,2 +1,24 @@
-class Api::V1::Auth::SessionsController < DeviseTokenAuth::ApplicationController
+class Api::V1::Auth::SessionsController < DeviseTokenAuth::SessionsController
+  # def sign_in_params
+  #   params.permit(:email, :password)
+  # end
+
+  def create
+    params.permit(:email, :password)
+    render json: current_user
+  end
+
+  # protected
+
+  #   def valid_params?(key, val)
+  #     # resource_params[:name] && key && val
+  #     resource_params[:email] && key && val
+  #     # params.permit(:email, :password)
+  #   end
+
+  private
+
+    def resource_params
+      params.permit(:password, :login)
+    end
 end
--- spec/requests/api/v1/auth/sessions_request_spec.rb
+++ spec/requests/api/v1/auth/sessions_request_spec.rb
@@ -1,16 +1,25 @@
 require "rails_helper"
 
+# $ bundle exec rspec spec/requests/api/v1/auth/sessions_request_spec.rb --tag focus
 RSpec.describe "Api::V1::Auth::Sessions", type: :request do
+  binding.pry
   describe "POST /api/v1/auth/:id" do
     subject { post(api_v1_user_session_path, params: params) }
 
     context "登録してるアカウントでログインする時" do
-      it "login succeeds" do
+      let(:params) { attributes_for(:user) }
+
+      fit "login succeeds" do
+        subject
+        binding.pry
       end
     end
 
     context "登録されていないアカウントでログインしようとする時" do
+      let(:params) { attributes_for(:user) }
+
       it "do Error" do
+        subject
       end
     end
   end
