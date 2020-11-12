42c2661edd869935f0da71cac360e1cd70ad084d
KazusaNakagawa <kazusanakgawa@gmail.com>
2020-11-12 22:05:08 +0900

    debug部分を削除

--- spec/requests/api/v1/auth/sessions_request_spec.rb
+++ spec/requests/api/v1/auth/sessions_request_spec.rb
@@ -2,16 +2,14 @@ require "rails_helper"
 
 # $ bundle exec rspec spec/requests/api/v1/auth/sessions_request_spec.rb --tag focus
 RSpec.describe "Api::V1::Auth::Sessions", type: :request do
-  binding.pry
   describe "POST /api/v1/auth/:id" do
     subject { post(api_v1_user_session_path, params: params) }
 
     context "登録してるアカウントでログインする時" do
       let(:params) { attributes_for(:user) }
 
-      fit "login succeeds" do
+      it "login success" do
         subject
-        binding.pry
       end
     end
 
