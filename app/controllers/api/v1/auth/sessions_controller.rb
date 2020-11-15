f1be733f3e2eddab61c50697bb42b24f5b09324c
KazusaNakagawa <kazusanakgawa@gmail.com>
2020-11-15 15:25:44 +0900

    fix sessions controller

--- app/controllers/api/v1/auth/sessions_controller.rb
+++ app/controllers/api/v1/auth/sessions_controller.rb
@@ -1,18 +1,3 @@
 class Api::V1::Auth::SessionsController < DeviseTokenAuth::SessionsController
-  # skip_before_action :authenticate_user!, only: [:create, :destroy]
   skip_before_action :verify_authenticity_token
-  # protect_from_forgery with: :null_session
-
-  # client_id = request.headers['client']
-
-  protected
-
-    def valid_params?(key, val)
-      # binding.pry
-      params[:session] && key && val
-      # params.permit(:email, :password, :session)
-
-      # origin -----------------
-      # resource_params[:password] && key && val
-    end
 end
