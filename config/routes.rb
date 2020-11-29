f52a67c1e506ca574ed8b3b2deb2d4e8c98d7ab5
KazusaNakagawa <kazusanakgawa@gmail.com>
2020-11-29 18:41:49 +0900

    コメントを反映

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,11 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      # 既存のクラスをorverrideで使う. 再発明しない
       mount_devise_token_auth_for "User", at: "auth", controllers: {
         registrations: "api/v1/auth/registrations",
       }
+      # REST API 紐付ける
       resources :articles
     end
   end
