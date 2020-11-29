b0a85b51ff4dd4d24642db62f93d470ff444eb8e
KazusaNakagawa <kazusanakgawa@gmail.com>
2020-11-29 17:55:56 +0900

    fix: aias_methodを使用

--- app/controllers/api/v1/base_api_controller.rb
+++ app/controllers/api/v1/base_api_controller.rb
@@ -1,16 +1,5 @@
 class Api::V1::BaseApiController < ApplicationController
-  def current_user
-    # """ 認証Userをインスタン変数に格納 """
-    @current_user ||= current_api_v1_user
-  end
-
-  def authenticate_user!
-    # """ ログインしていないと使えない API
-    # 記事: 作成, 更新, 削除
-    # 認証Userであるか。 認証Userでなければ 401 """
-  end
-
-  def user_signed_in?
-    # """ User が sign in しているか """
-  end
+  alias_method :current_user, :current_api_v1_user
+  alias_method :authenticate_user!, :authenticate_api_v1_user!
+  alias_method :user_signed_in?, :api_v1_user_signed_in?
 end
