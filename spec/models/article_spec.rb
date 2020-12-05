ece0f5820b07c64c4119a89eacdd8389e149ce8f
KazusaNakagawa <kazusanakgawa@gmail.com>
2020-12-05 14:16:09 +0900

    Article model: 正常系テストの追加・修正

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -21,39 +21,70 @@
 require "rails_helper"
 
 RSpec.describe Article, type: :model do
-  context "titleがある時" do
-    let(:article) { create(:article) }
+  describe "正常系" do
+    context "titleがある時" do
+      let(:article) { create(:article) }
 
-    it "記事を投稿できる" do
-      expect(article).to be_valid
+      it "記事を投稿できる" do
+        expect(article).to be_valid
+      end
+    end
+
+    context "記事作成で, statusを指定しない時" do
+      let(:article) { create(:article) }
+
+      it "default で下書き記事が作成される" do
+        expect(article).to be_valid
+        expect(article.status).to eq "drafts"
+      end
+    end
+
+    context "下書き選択で記事を作成した時" do
+      let(:article) { create(:article, :drafts) }
+
+      it "下書き記事が作成される" do
+        expect(article).to be_valid
+        expect(article.status).to eq "drafts"
+      end
+    end
+
+    context "公開記事で作成する時" do
+      let(:article) { create(:article, status: :published) }
+
+      it "公開記事で作成される" do
+        expect(article).to be_valid
+        expect(article.status).to eq "published"
+      end
     end
   end
 
-  context "title が 10文字より少ない時" do
-    let(:article) { build(:article, title: "*" * 9) }
+  describe "異常系" do
+    context "title が 10文字より少ない時" do
+      let(:article) { build(:article, title: "*" * 9) }
 
-    it "記事の投稿に失敗する" do
-      expect(article).to be_invalid
-      expect(article.errors.details[:title][0][:error]).to eq :too_short
+      it "記事の投稿に失敗する" do
+        expect(article).to be_invalid
+        expect(article.errors.details[:title][0][:error]).to eq :too_short
+      end
     end
-  end
 
-  context "title が 50文字より多い時" do
-    let(:article) { build(:article, title: "*" * 51) }
+    context "title が 50文字より多い時" do
+      let(:article) { build(:article, title: "*" * 51) }
 
-    it "記事の投稿に失敗する" do
-      expect(article).to be_invalid
-      expect(article.errors.details[:title][0][:error]).to eq :too_long
+      it "記事の投稿に失敗する" do
+        expect(article).to be_invalid
+        expect(article.errors.details[:title][0][:error]).to eq :too_long
+      end
     end
-  end
 
-  context "titleがない時" do
-    let(:article) { build(:article, title: nil) }
+    context "titleがない時" do
+      let(:article) { build(:article, title: nil) }
 
-    it "記事の投稿に失敗する" do
-      expect(article).to be_invalid
-      expect(article.errors.details[:title][0][:error]).to eq :blank
-      expect(article.errors.details[:title][1][:error]).to eq :too_short
+      it "記事の投稿に失敗する" do
+        expect(article).to be_invalid
+        expect(article.errors.details[:title][0][:error]).to eq :blank
+        expect(article.errors.details[:title][1][:error]).to eq :too_short
+      end
     end
   end
 end
