8475caf3f93d4e90b403ff156326e3f0b2bd1c68
KazusaNakagawa <kazusanakgawa@gmail.com>
2020-12-10 23:35:32 +0900

    current_userで処理してなかったので修正

--- app/controllers/api/v1/articles/draft_controller.rb
+++ app/controllers/api/v1/articles/draft_controller.rb
@@ -1,20 +1,20 @@
 module Api::V1
   class Articles::DraftController < ApplicationController
     def index
-      # """ 下書き一覧が確認できる処理 """"
+      # """ 自身の下書き一覧が確認できる処理 """"
       # GET: http://localhost:3000/api/v1/articles/draft
 
-      articles = Article.where(status: 0).order(updated_at: :desc)
+      articles = current_api_v1_user.articles.where(status: 0).order(updated_at: :desc)
       render json: articles, each_serializer: Api::V1::ArticlePreviewSerializer
     end
 
     def show
-      # """ 選択した下書き１レコードが確認できる処理 """
+      # """ 自身の下書き１レコードが確認できる処理 """
       # GET: http://localhost:3000/api/v1/articles/draft/:id
 
-      article = Article.find(params[:id])
+      article = current_api_v1_user.articles.find(params[:id])
 
-      # TODO: 登録されていないレコードが指定された時, Not Found で返すmメッセージ
+      # TODO: 登録されていないレコードが指定された時, Not Found で返すメッセージ
 
       # 公開記事が指定された場合は Not Found で返す
       if article["status"] == "published"
