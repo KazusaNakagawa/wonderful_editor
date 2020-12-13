6fef97c78855aa53e8107fa1a09a111bc3269dc2
KazusaNakagawa <kazusanakgawa@gmail.com>
2020-12-13 09:07:48 +0900

    前処理の呼び出し順をリファクタリング

--- spec/requests/api/v1/articles/draft_request_spec.rb
+++ spec/requests/api/v1/articles/draft_request_spec.rb
@@ -3,12 +3,14 @@ require "rails_helper"
 # $ bundle exec rspec spec/requests/api/v1/articles/draft_request_spec.rb --tag focus
 RSpec.describe "Api::V1::Articles::Drafts", type: :request do
   describe "GET /api/v1/articles/draft" do
+    subject { get(api_v1_articles_draft_index_path, headers: headers) }
+
+    # 登録Userでログイン
+    let(:current_user) { create(:user) }
+    let(:headers) { current_user.create_new_auth_token }
+
     context "自身の下書き一覧を確認する時" do
-      subject { get(api_v1_articles_draft_index_path, headers: headers) }
-
-      let(:current_user) { create(:user) }
-      let(:headers) { current_user.create_new_auth_token }
-
+      # 記事を下書きで作成
       let!(:article1) { create(:article, :draft, user: current_user, updated_at: 1.days.ago) }
       let!(:article2) { create(:article, :draft, user: current_user, updated_at: 2.days.ago) }
       let!(:article3) { create(:article, :draft, user: current_user) }
