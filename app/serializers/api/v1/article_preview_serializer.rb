4b20ce19b8c7ecb32d29649eeaa9b52528965695
KazusaNakagawa <kazusanakgawa@gmail.com>
2020-12-15 01:33:25 +0900

    serializerの修正: 詳細記事が表示されていなかった

--- app/serializers/api/v1/article_preview_serializer.rb
+++ app/serializers/api/v1/article_preview_serializer.rb
@@ -1,6 +1,6 @@
 class Api::V1::ArticlePreviewSerializer < ActiveModel::Serializer
-  # 値を返したい column を指定する
-  attributes :id, :title, :status, :updated_at
+  # 一覧 column を指定する
+  attributes :id, :title, :updated_at
 
   # userを関連付ける
   belongs_to :user, serializer: Api::V1::UserSerializer
