be436330171fd72d946cb3964e5d64560efea65f
KazusaNakagawa <kazusanakgawa@gmail.com>
2020-12-20 13:11:32 +0900

    いいね機能を実装するための controller を生成

---
+++ app/controllers/api/v1/articles/article_likes_controller.rb
@@ -0,0 +1,17 @@
+module Api::V1
+  class Articles::ArticleLikesController < BaseApiController
+    before_action :authenticate_user!
+
+    def create
+      # """ いいねする """
+      article_like = current_user.article_likes.create!(article_id: params[:article_id])
+      render json: article_like
+    end
+
+    def destroy
+      # """ いいね取り消す """
+      article_like = current_user.article_likes.find(params[:id])
+      article_like.destroy!
+    end
+  end
+end
